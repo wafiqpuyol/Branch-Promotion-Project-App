4ac64fe41846f0396e0818257e2a6e00f906abae
wafiqpuyol <wafiqgithub775@gmail.com>
2026-07-30 22:15:28 +0600

    add express server

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        environment: process.env.ENV_NAME,
+        version: "3.0.0",
+        message: `Hello from ${process.env.ENV_NAME}`,
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
